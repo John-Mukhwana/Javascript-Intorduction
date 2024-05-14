30feb1aae5ca32bb1cc655d890b60990a82874f9
AJD254 <jontexjesus@gmail.com>
2024-05-14 06:50:19 +0300

    Intro to javascript

---
+++ Intro.js
@@ -0,0 +1,16 @@
+/*This is My Introduction to javascript*/
+//Variables declaration and assigning
+var firstName="John";
+var lastName="Mukhwana";
+let fullName=firstName +  lastName;
+console.log(fullName)
+/*Control Structures*/
+//var Name = prompt("ENTER NUMBER A:");
+var a=10;
+var b=30;
+if (a>b){
+    console.log("a is greater than b");
+}
+else{
+    console.log("b is greater than a")
+}
