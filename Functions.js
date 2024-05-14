884aa78c8c133ac7adc1e70265a623891c25a988
AJD254 <jontexjesus@gmail.com>
2024-05-14 11:20:47 +0300

    Added functions

---
+++ Functions.js
@@ -0,0 +1,20 @@
+//functions
+//1.Normal function
+/*function great(){
+    console.log("Hello world")
+}
+//function call
+great();*/
+
+//call
+
+
+let greatMeArrow = (name) =>{
+    console.log(name);
+};
+greatMeArrow("John");
+
+function greet(name){
+    console.log(name);
+};
+greet("Papa");
