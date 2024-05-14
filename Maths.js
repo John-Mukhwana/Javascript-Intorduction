d6e880938652015afbb6b72359e91ab671aaf444
AJD254 <jontexjesus@gmail.com>
2024-05-14 11:27:39 +0300

    Math in Js

---
+++ Maths.js
@@ -0,0 +1,14 @@
+
+//Math Objects.
+console.log(Math.PI);
+//Mathe Methods
+console.log(Math.sqrt(3));
+console.log(Math.pow(2,3));
+console.log(Math.floor(10.4));
+console.log(Math.ceil(10.5));
+console.log(Math.max(10,20,40,80));
+console.log(Math.min(5,20,10,69,80,70));
+console.log(Math.random());
+console.log(Math.sin(0.0482));
+console.log(Math.cos(0.0482));
+console.log(Math.tan(0.482));
