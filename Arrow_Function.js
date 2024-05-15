cfd24e62204e0f35ef2b4c2db841d8bcc482e375
AJD254 <jontexjesus@gmail.com>
2024-05-15 22:53:23 +0300

    Learned difference between normal function and arrow function

---
+++ Arrow_Function.js
@@ -0,0 +1,14 @@
+/*makes code more readable*/
+
+//NORMAL FUNCTION
+function myFunction(name){
+    console.log(`hello, ${name} This is a normal function`);
+}
+myFunction('said');
+
+//ES6 ARROW  FUNCTION
+const myfunction2= (name2)=>{
+    console.log(`hello,${name2} this is ES6 Arrow function`);
+}
+
+myfunction2('Bradley')
